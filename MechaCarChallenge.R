a1c9842fe7dc83044bfa34be4ecb9d98ce66c9f3
Kim J Kubik <kkubik7@hotmail.com>
2022-02-04 22:25:01 -0600

    Challenge 2 Delivery

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,7 @@
-# Challenge 1 : Linear Regression to Predict MPG
+######################################################
+# Deliverable 1 :  Linear Regression to Predict MPG  #
+######################################################
+
 #install.packages("tidyverse")
 #install.packages("dplyr")
 
@@ -11,3 +14,15 @@ num_rows <- 1:nrow(mechacar_df)
 # and r-squared value for the model
 summary(lm( mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_df)) 
 
+##############################################################
+# Deliverable 2: Create Visualizations for the Trip Analysis #
+##############################################################
+# read open input file: 
+suspend_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+library(tidyverse)
+
+# get mean, median, variance and standard deviation (no grouping)
+total_summary <- suspend_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
+
+# mean, median, variance and standard deviation (group by Manufacturing Lot)
+lot_summary <- suspend_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
